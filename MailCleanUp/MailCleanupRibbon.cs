79246bf3757d8147aa8240484d15e64004e43356
Devin Turner <turnerd18@gmail.com>
2015-01-14 07:45:58 -0600

    adding ribbon code file

---
+++ MailCleanUp/MailCleanupRibbon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace MailCleanUp
+{
+    public partial class MailCleanupRibbon
+    {
+        private void MailCleanupRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void MaxItemsBox_TextChanged(object sender, RibbonControlEventArgs e)
+        {
+            var inte = int.Parse(((RibbonEditBox) sender).Text);
+        }
+
+        private void StartCleanupButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            int maxItemsToDelete;
+            if (int.TryParse(MaxItemsBox.Text, out maxItemsToDelete))
+            {
+                Globals.ThisAddIn.DeleteOldMessages(maxItemsToDelete);
+            }
+        }
+    }
+}
